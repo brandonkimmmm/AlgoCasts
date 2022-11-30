ea2dd3fc296a618cf1792d6da918c273b9f7588c
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-11-30 19:09:02 +0900

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,36 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+	let result = 0;
+	const isNegative = n < 0;
+	n = Math.abs(n);
+
+	while (n !== 0) {
+		if (result > 0) result *= 10;
+		result += n % 10;
+		n = Math.floor(n / 10);
+	}
+
+	return isNegative ? result * -1 : result;
+}
+
+// function reverseInt(n) {
+// 	let splitInt = n.toString().split('');
+// 	let isNegative = false;
+
+// 	if (splitInt[0] === '-') {
+// 		isNegative = true;
+// 		splitInt.slice(1);
+// 	}
+
+// 	for (let i = 0; i < splitInt.length / 2; i++) {
+// 		const temp = splitInt[i];
+// 		splitInt[i] = splitInt[splitInt.length - i - 1];
+// 		splitInt[splitInt.length - i - 1] = temp;
+// 	}
+
+// 	return isNegative ? parseInt(splitInt.join('')) * -1 : parseInt(splitInt.join(''));
+// }
 
 module.exports = reverseInt;
