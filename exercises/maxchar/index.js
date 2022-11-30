03d851f51b5d160b0de4ff789357faae592f0a74
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-11-30 19:14:58 +0900

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	const dict = {};
+
+	for (const char of str) {
+		dict[char] = (dict[char] || 0) + 1;
+	}
+
+	let max = 0;
+	let maxChar = '';
+
+	for (const char in dict) {
+		if (dict[char] > max) {
+			max = dict[char];
+			maxChar = char;
+		}
+	}
+
+	return maxChar;
+}
 
 module.exports = maxChar;
