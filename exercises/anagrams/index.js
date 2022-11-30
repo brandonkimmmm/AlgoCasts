78118040cd4e43cd7a8266b0e933cb847009a0ae
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-11-30 19:39:12 +0900

    anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,34 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const buildCharDict = (str) => {
+	const regex = new RegExp(/\w/);
+	const dict = {};
+
+	for (const char of str) {
+		if (regex.test(char)) {
+			dict[char.toLowerCase()] = (dict[char.toLowerCase()] || 0) + 1;
+		}
+	}
+
+	return dict;
+}
+
+function anagrams(stringA, stringB) {
+	const dictA = buildCharDict(stringA);
+	const dictB = buildCharDict(stringB);
+
+	if (Object.keys(dictA).length !== Object.keys(dictB).length) {
+		return false;
+	}
+
+	for (const char in dictA) {
+		if (dictA[char] !== dictB[char]) {
+			return false;
+		}
+	}
+
+	return true;
+}
 
 module.exports = anagrams;
