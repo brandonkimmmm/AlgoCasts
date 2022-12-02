e742e47d450f5943b10757c6824f7c5810789d1a
swkoyo <10441291+swkoyo@users.noreply.github.com>
2022-12-02 16:52:05 +0900

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,25 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+	const columns = n * 2 - 1;
+	const mid = Math.floor(columns / 2);
+
+	for (let i = 0; i < n; i++) {
+		let row = '';
+		const leftIndex = mid - i;
+		const rightIndex = mid + i;
+
+		for (let j = 0; j < columns; j++) {
+			if (j < leftIndex || j > rightIndex) {
+				row += ' ';
+			} else {
+				row += '#';
+			}
+		}
+
+		console.log(row);
+	}
+}
 
 module.exports = pyramid;
